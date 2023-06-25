b8ace81f8d7927e62d17b3f16401effb227a45fd
Victor Hugo <pavictorhugo@gmail.com>
2023-06-25 18:11:10 -0300

    Fix to param

--- routes/convert.tsx
+++ routes/convert.tsx
@@ -17,7 +17,7 @@ export const handler: Handlers<Data> = {
     const amount = Number(url.searchParams.get('amount')) || 0;
     const from = url.searchParams.get('from') || DEFAULT_CURRENCY; 
     const to = url.searchParams.get('to') || DEFAULT_CURRENCY;
-    const csResponse = await fetch(`https://api.currencyscoop.com/v1/convert?api_key=${Deno.env.get('API_KEY')}&from${from}&to=${to}&amount=${amount}`)
+    const csResponse = await fetch(`https://api.currencyscoop.com/v1/convert?api_key=${Deno.env.get('API_KEY')}&from=${from}&to=${to}&amount=${amount}`)
     const csResult = await csResponse.json();
     return ctx.render({
       convertedAmount: csResult.response.value, amount, from, to
